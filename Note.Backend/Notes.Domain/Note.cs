149d9f5aeca4889f4138247ea623f54fdb3ac510
StandShiba <korneev092005@yandex.ru>
2026-01-12 14:21:52 +0300

    7

--- Note.Backend/Notes.Domain/Note.cs
+++ Note.Backend/Notes.Domain/Note.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Notes.Domain
+﻿namespace Notes.Domain
 {
-    internal class Note
+    public class Note
     {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
     }
 }
